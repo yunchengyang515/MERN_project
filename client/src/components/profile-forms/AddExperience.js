d472f0ab6714693826e56274583bc68309c1b4b4
FabioYyc <45674168+FabioYyc@users.noreply.github.com>
2020-01-18 15:30:03 +1100

    made the select requried attribute with material ui

--- client/src/components/profile-forms/AddExperience.js
+++ client/src/components/profile-forms/AddExperience.js
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 import { addFightExperience } from '../../actions/profile';
 import { Link, withRouter } from 'react-router-dom';
 import styled from 'styled-components';
-import { Container, Paper, Select, MenuItem, FormControl, Typography} from '@material-ui/core';
+import { Container, Paper, Typography, TextField } from '@material-ui/core';
 import Alert from '../layout/Alert';
 //Todo:
 //1: fix the height
@@ -29,7 +29,10 @@ const ContainerWrapper = styled(Container)`
 	width: 100%;
 	height: 100%;
 `;
-
+const SelectWrapper = styled(TextField)`
+width:70% !important;
+border
+`;
 const AddExperience = ({ addFightExperience, history }) => {
 	const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
 	const onSubmit = e => {
@@ -47,19 +50,19 @@ const AddExperience = ({ addFightExperience, history }) => {
 
 	return (
 		<section class="register">
-			<Alert/>
+			<Alert />
 			<div class="dark-overlay">
 				<ComponentWrapper>
 					<ContainerWrapper>
-						<Alert/>
+						<Alert />
 						<h1 class="large text-primary">Add your fight experience</h1>
 						<p class="lead">
 							<i class="fas fa-user"></i> Fight experience
 						</p>
 						<form class="form" onSubmit={e => onSubmit(e)}>
 							<div class="form-group">
-							<Typography >Which promotion the fight belongs to</Typography>
-								<input
+								<Typography>Which promotion the fight belongs to</Typography>
+								<SelectWrapper
 									type="text"
 									placeholder="Fight Promotion"
 									value={promotion}
@@ -67,31 +70,57 @@ const AddExperience = ({ addFightExperience, history }) => {
 									onChange={e => onChange(e)}
 								/>
 							</div>
-							<div class = "form-group" >
-							<Typography >What is the discipline of the fight</Typography>
-								<Select name="discipline" value={discipline} onChange={e => onChange(e)} required>
-									<MenuItem value="Muay Thai">Muay Thai</MenuItem>
-									<MenuItem value="MMA">MMA</MenuItem>
-									<MenuItem value="Boxing">Boxing</MenuItem>
-									<MenuItem value="Others">Others</MenuItem>
-								</Select>
+							<Typography>* What is the discipline of the fight</Typography>
+							<SelectWrapper
+								name="discipline"
+								select
+								required
+								value={discipline}
+								onChange={e => onChange(e)}
+								SelectProps={{
+									native: true,
+								}}
+							>
+								<option />
+								<option value="Muay Thai">Muay Thai</option>
+								<option value="MMA">MMA</option>
+								<option value="Boxing">Boxing</option>
+								<option value="Others">Others</option>
+							</SelectWrapper>
+							<div class="form-group">
+								<Typography>* Is the fight a pro fight?</Typography>
+								<SelectWrapper
+									name="isPro"
+									select
+									required
+									value={isPro}
+									onChange={e => onChange(e)}
+									SelectProps={{
+										native: true,
+									}}
+								>
+									<option />
+									<option value={true}>Yes</option>
+									<option value={false}>No</option>
+								</SelectWrapper>
 							</div>
 							<div class="form-group">
-							<Typography >Is the fight a pro fight?</Typography>
-								<Select name="isPro" value={isPro} onChange={e => onChange(e)}>
-									<MenuItem value={true}>Yes</MenuItem>
-									<MenuItem value={false}> No </MenuItem>
-								</Select>
-							</div>
-							<div class="form-group">
-							<Typography >Reuslt of the fight</Typography>
-								<Select name="result" value={result} onChange={e => onChange(e)}>
-									<MenuItem value={'Win by points'}>Win by points</MenuItem>
-									<MenuItem value={'Win by KO/TKO'}>Win by KO/TKO</MenuItem>
-									<MenuItem value={'Lose'}> Lose </MenuItem>
-									<MenuItem value={'Draw'}> Draw </MenuItem>
-								</Select>
-								
+								<Typography>Reuslt of the fight</Typography>
+								<SelectWrapper
+									select
+									SelectProps={{
+										native: true,
+									}}
+									name="result"
+									value={result}
+									onChange={e => onChange(e)}
+								>
+									<option />
+									<option value={'Win by points'}>Win by points</option>
+									<option value={'Win by KO/TKO'}>Win by KO/TKO</option>
+									<option value={'Lose'}> Lose </option>
+									<option value={'Draw'}> Draw </option>
+								</SelectWrapper>
 							</div>
 							<input type="submit" class="btn btn-primary" value="Add fight experience" />
 						</form>
